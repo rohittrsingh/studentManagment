d26fe82118ceef4211605633e603f036118bcffa
Rohit <rohittrsingh@gmail.com>
2023-06-28 21:22:51 +0530

    add delete func

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from .apis import SchoolView, StudentView, SchoolStudentView
-from .views import school_register_view, student_register_view, school_edit_view
+from .views import school_register_view, student_register_view, school_edit_view, school_delete_view, \
+    student_delete_view
 
 urlpatterns = [
     path('api/school/<int:id>', SchoolView.as_view()),
     path('api/student/<int:id>', StudentView.as_view()),
     path("api/all_schools_details", SchoolStudentView.as_view()),
     path("create_school", school_register_view),
-    path("create_student",student_register_view),
-    path("edit-school/<int:_id>", school_edit_view)
+    path("create_student", student_register_view),
+    path("edit-school/<int:_id>", school_edit_view),
+    path("delete_student/<int:_id>", student_delete_view),
+    path("delete_School/<int:_id>", school_delete_view)
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .forms import SchoolForm, StudentForm
-from .models import School
+from .models import School, Student
 
 
 def school_register_view(request):
@@ -49,13 +49,6 @@ def school_edit_view(request, _id):
         form = SchoolForm(instance=school)
         return render(request, "create_school.html", {"form": form})
 
-    # school = get_object_or_404(School, id=_id)
-    #
-    # if request.method == 'GET':
-    #     context = {'form': SchoolForm(instance=school), 'id': _id}
-    #     return render(request, 'create_school.html', context)
-
-
 
 def student_edit_view(request):
     if request.method == "POST":
@@ -68,3 +61,20 @@ def student_edit_view(request):
     else:
         form = StudentForm()
         return render(request, "create_school.html", {"form": form})
+
+
+def student_delete_view(request, _id):
+    student = Student.objects.get(id=_id)
+    student.delete()
+    return render(request, "submitted.html")
+
+
+def school_delete_view(request, _id):
+    school = School.objects.get(id=_id)
+    school.delete()
+    return render(request, "submitted.html")
+
+
+def get_student_view(request, _id):
+    student = Student.objects.get(id=_id)
+    return render(request, "submitted.html")
